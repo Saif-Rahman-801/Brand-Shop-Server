0f6da9a0bb1db3e0c99f3446615171cf5a15f455
Saif-Rahman-801 <saifurs102@gmail.com>
2023-10-20 19:36:32 +0600

    data deleted from cart

--- index.js
+++ index.js
@@ -82,6 +82,14 @@ async function run() {
       res.send(result);
     });
 
+    // Delete data
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await CartCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
